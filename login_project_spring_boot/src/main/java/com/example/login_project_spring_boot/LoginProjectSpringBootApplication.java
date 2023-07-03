6eab9ddae08908477505a342c75c7429d086a324
kyj <xenexes@naver.com>
2023-07-04 00:17:01 +0900

    login 기능으로 세션확인하는 스프링 부트버전 프로젝트입니다.

---
+++ login_project_spring_boot/src/main/java/com/example/login_project_spring_boot/LoginProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_project_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProjectSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginProjectSpringBootApplication.class, args);
+    }
+
+}
---
+++ login_project_spring_boot/src/test/java/com/example/login_project_spring_boot/LoginProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_project_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginProjectSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
