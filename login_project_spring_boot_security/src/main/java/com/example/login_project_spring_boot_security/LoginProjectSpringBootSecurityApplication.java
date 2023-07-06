62d67082d5345e100c45321ab88ee9dda0252654
xenex90 <xenexes@naver.com>
2023-07-06 16:31:40 +0900

    스프링 시큐리티 첫 커밋 다시 푸쉬

---
+++ login_project_spring_boot_security/src/main/java/com/example/login_project_spring_boot_security/LoginProjectSpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_project_spring_boot_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProjectSpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginProjectSpringBootSecurityApplication.class, args);
+	}
+
+}
