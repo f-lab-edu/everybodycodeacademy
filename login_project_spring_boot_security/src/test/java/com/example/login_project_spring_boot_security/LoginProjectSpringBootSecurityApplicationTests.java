ee67f1fd319feec3938507af3770bf5e390123ee
xenex90 <xenexes@naver.com>
2023-07-06 15:40:00 +0900

    스프링 시큐리티 첫 커밋

---
+++ login_project_spring_boot_security/src/test/java/com/example/login_project_spring_boot_security/LoginProjectSpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_project_spring_boot_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginProjectSpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
