2af1038c4a8f84edba30c8d3a3df68c2e8f52451
YoungJu-Gim <56420135+xenex90@users.noreply.github.com>
2023-07-06 21:03:28 +0900

    Login project spring boot security (#6)
    * 테스트용 아이디/비밀번호로 변경
    
    * .gitignore 파일 추가
    
    * .gitignore 파일 수정
    
    * 스프링 프레임워크 프로젝트 로그인으로 세션 확인 첫 커밋
    
    * login 기능으로 세션확인하는 스프링 부트버전 프로젝트입니다.
    
    * .gitignore 파일 수정 - .idea폴더도 포함시키지 않도록 수정
    
    * 스프링 시큐리티 첫 커밋
    
    * Revert "스프링 시큐리티 첫 커밋"
    
    This reverts commit ee67f1fd319feec3938507af3770bf5e390123ee.
    
    * Revert "스프링 시큐리티 첫 커밋"
    
    This reverts commit ee67f1fd319feec3938507af3770bf5e390123ee.
    
    * 스프링 시큐리티 첫 커밋 다시 푸쉬
    
    * Fix .gitignore file conflict
    
    .idea 폴더는 커밋 되도록 설정되어 있어서
    .idea 폴더도 커밋되지 않도록 .gitignore 파일 수정

---
+++ login_project_spring_boot_security/src/main/java/com/example/login_project_spring_boot_security/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.login_project_spring_boot_security;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LoginProjectSpringBootSecurityApplication.class);
+	}
+
+}
