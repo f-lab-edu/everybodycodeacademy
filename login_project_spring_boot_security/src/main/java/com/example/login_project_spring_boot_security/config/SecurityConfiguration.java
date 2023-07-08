0ceed61c1f7255d601e1d9dc9693fe8cd2bab619
xenex90 <xenexes@naver.com>
2023-07-08 23:14:48 +0900

    Add userDetailsService method
    userDetailsService 메소드 추가
    admin, user UserDetails 객체 추가

--- login_project_spring_boot_security/src/main/java/com/example/login_project_spring_boot_security/config/SecurityConfiguration.java
+++ login_project_spring_boot_security/src/main/java/com/example/login_project_spring_boot_security/config/SecurityConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,5 +17,14 @@ public class SecurityConfiguration {
     http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults());
     return http.build();
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails admin =
+            User.builder().username("admin").password("admin").roles("ADMIN").build();//admin
+    UserDetails user =
+            User.builder().username("test").password("test").roles("USER").build();//user
+    return new InMemoryUserDetailsManager(admin, user);
   }
 }
