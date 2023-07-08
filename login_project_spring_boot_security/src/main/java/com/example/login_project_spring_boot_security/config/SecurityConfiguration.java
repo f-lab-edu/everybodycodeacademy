6a185c6fd7cd9cad85e2238743d28ef23e4bf1ed
xenex90 <xenexes@naver.com>
2023-07-08 20:12:54 +0900

    Add SecurityConfiguration.java
    SecurityConfiguration 클래스를 추가하였고 이 클래스에
    filterChain 메소드를 추가하였습니다.
    HttpSecurity를 매개변수로 받아 SecurityFilterChain을 리턴하는
    메소드 입니다.
    이 메소드는 HttpSecurity를 이용하여 서버에게로의 접근을 제어하는 기능을
    가지고 있습니다.

---
+++ login_project_spring_boot_security/src/main/java/com/example/login_project_spring_boot_security/config/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.login_project_spring_boot_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+        .httpBasic(Customizer.withDefaults());
+    return http.build();
+  }
+}
