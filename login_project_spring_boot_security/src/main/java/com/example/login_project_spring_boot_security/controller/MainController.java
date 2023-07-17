55d4dbaa45a8d131930d6ca06eec8721eafa2dc6
xenex90 <xenexes@naver.com>
2023-07-17 22:21:16 +0900

    Add login fun by spring security 6.1.1
    스프링 시큐리티 6.1.1 버전으로 로그인 기능을 구현한 구현중인 프로젝트의 일부 부분입니다.
    
    Configuration에서 로그인 화면 주소, 로그인 프로세스 주소, 성공시 리다이렉트 주소를 설정하고
    MemberPrincipalDetails와 MemberPrincipalDetailsService를 UserDetails와 UserDetailsService 각각 상속 받아서 구현하였습니다.
    성공한 리다이렉트 주소에서 Principal 객체로 인증 유저 정보를 model로 담아 보내서 이동한 화면에서 유저 이름, 이메일 등을 보여줍니다.
    핸들러는 실패 핸들러(MemberAuthFailureHandler)는 동작하는 걸로 알고 있고, 성공시의 핸들러(MemberAuthSuccessHandler)는 작성만 해두었습니다.
    Member(entity)와 AuthenticationProvider부분을 구현한 소스도 있습니다.
    
    이외에 DB 연결을 위한 설정파일을 수정하였고,
    기존 Configuration부분 소스(연습)은 삭제하였습니다.

--- login_project_spring_boot_security/src/main/java/com/example/login_project_spring_boot_security/controller/MainController.java
+++ login_project_spring_boot_security/src/main/java/com/example/login_project_spring_boot_security/controller/MainController.java
@@ -1,8 +1,6 @@
 package com.example.login_project_spring_boot_security.controller;
 
-import com.example.login_project_spring_boot_security.member.entity.Member;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
