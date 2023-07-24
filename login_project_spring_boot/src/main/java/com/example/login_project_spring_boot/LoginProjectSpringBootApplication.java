945e70fabd5d8996bed9530c9dd4d5d5b26309f3
xenex90 <xenexes@naver.com>
2023-07-24 11:36:36 +0900

    Generate war and deploy in tomcat commit
    현재까지 수정한 내용 그대로 수정없이 다 그대로 올립니다.
    
    주로 수정한 소스는
    LoginProjectSpringBootSecurityApplication Class,
    pom.xml 파일입니다.

--- login_project_spring_boot/src/main/java/com/example/login_project_spring_boot/LoginProjectSpringBootApplication.java
+++ login_project_spring_boot/src/main/java/com/example/login_project_spring_boot/LoginProjectSpringBootApplication.java
@@ -2,9 +2,15 @@ package com.example.login_project_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LoginProjectSpringBootApplication {
+public class LoginProjectSpringBootApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LoginProjectSpringBootApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LoginProjectSpringBootApplication.class, args);
--- login_project_spring_boot_security/src/main/java/com/example/login_project_spring_boot_security/LoginProjectSpringBootSecurityApplication.java
+++ login_project_spring_boot_security/src/main/java/com/example/login_project_spring_boot_security/LoginProjectSpringBootSecurityApplication.java
@@ -2,9 +2,11 @@ package com.example.login_project_spring_boot_security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LoginProjectSpringBootSecurityApplication {
+public class LoginProjectSpringBootSecurityApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginProjectSpringBootSecurityApplication.class, args);
