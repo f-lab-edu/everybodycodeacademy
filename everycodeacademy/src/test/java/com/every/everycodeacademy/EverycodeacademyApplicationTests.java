e1550ac98ed87e480b2b48ab9f6f649c02d9a0a8
xenex90 <xenexes@naver.com>
2023-07-28 16:17:40 +0900

    Initical codeacademy commit
    첫 프로젝트 커밋입니다.

---
+++ everycodeacademy/src/test/java/com/every/everycodeacademy/EverycodeacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.every.everycodeacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverycodeacademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
