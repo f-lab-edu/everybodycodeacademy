2d35ab84e435f4ba93d2ad8cd9bebb4a0f298c71
xenex90 <xenexes@naver.com>
2023-10-16 13:19:24 +0900

    move load Test Method & class
    - 로드 테스트 관련 메소드들 따로 class 생성 후 이동
    - 기존 소스코드 정렬

--- everycodeacademy/src/main/java/com/every/everycodeacademy/API/BoardAPI.java
+++ everycodeacademy/src/main/java/com/every/everycodeacademy/API/BoardAPI.java
@@ -9,9 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,16 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @ApiResponses({
-    @ApiResponse(responseCode = "200", description = "success"),
-    @ApiResponse(responseCode = "400", description = "bad request"),
-    @ApiResponse(responseCode = "404", description = "not found"),
-    @ApiResponse(responseCode = "500", description = "internal sever error")
+  @ApiResponse(responseCode = "200", description = "success"),
+  @ApiResponse(responseCode = "400", description = "bad request"),
+  @ApiResponse(responseCode = "404", description = "not found"),
+  @ApiResponse(responseCode = "500", description = "internal sever error")
 })
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/board")
 public class BoardAPI {
-
 
   private final BoardService boardService;
 
@@ -56,56 +53,4 @@ public class BoardAPI {
 
     return boardService.update(board);
   }
-
-  @PostMapping("/loadtest")
-  @Operation(summary = "load test 1 READ_UNCOMMITTED")
-  public void loadTest1(){
-
-    Board board = new Board();
-    boardService.loadTest1(board);
-    System.out.println("트랜잭션 READ_UNCOMMITTED 하나 완료");
-    boardService.loadTest2(board);
-    System.out.println("트랜잭션 READ_UNCOMMITTED 둘 완료");
-
-  }
-
-  @PostMapping("/loadtest2")
-  @Operation(summary = "load test 2 READ_COMMITTED")
-  @Transactional(isolation = Isolation.READ_COMMITTED)
-  public void loadTest2(){
-
-    Board board = new Board();
-    boardService.loadTest3(board);
-    System.out.println("트랜잭션 READ_COMMITTED 하나 완료");
-    boardService.loadTest4(board);
-    System.out.println("트랜잭션 READ_COMMITTED 둘 완료");
-
-  }
-
-
-  @PostMapping("/loadtest3")
-  @Operation(summary = "load test 3 REPEATABLE_READ")
-  @Transactional(isolation = Isolation.REPEATABLE_READ)
-  public void loadTest3(){
-
-      Board board = new Board();
-      boardService.loadTest5(board);
-      System.out.println("트랜잭션 REPEATABLE_READ 하나 완료");
-      boardService.loadTest6(board);
-      System.out.println("트랜잭션 REPEATABLE_READ 둘 완료");
-
-  }
-  @PostMapping("/loadtest4")
-  @Operation(summary = "load test 4 SERIALIZABLE")
-  @Transactional(isolation = Isolation.SERIALIZABLE)
-  public void loadTest4(){
-
-    Board board = new Board();
-    boardService.loadTest7(board);
-    System.out.println("트랜잭션 SERIALIZABLE 셋 완료");
-    boardService.loadTest8(board);
-    System.out.println("트랜잭션 SERIALIZABLE 넷 완료");
-
-  }
-
 }
--- everycodeacademy/src/main/java/com/every/everycodeacademy/board/BoardService.java
+++ everycodeacademy/src/main/java/com/every/everycodeacademy/board/BoardService.java
@@ -5,107 +5,31 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
-    private final BoardRepository boardRepository;
+  private final BoardRepository boardRepository;
 
-    public Board register(Board board) {
-       return boardRepository.save(board);
-    }
+  public Board register(Board board) {
+    return boardRepository.save(board);
+  }
 
-    public List<Board> list() {
-        return boardRepository.findAll(Sort.by(Sort.Direction.DESC, "idx"));
-    }
+  public List<Board> list() {
+    return boardRepository.findAll(Sort.by(Sort.Direction.DESC, "idx"));
+  }
 
-    public Board detail(int idx) {
-        return boardRepository.findById(idx).orElse(null);
-    }
+  public Board detail(int idx) {
+    return boardRepository.findById(idx).orElse(null);
+  }
 
-    public Board update(Board board) {
-        boardRepository.save(board);
-        return board;
-    }
+  public Board update(Board board) {
+    boardRepository.save(board);
+    return board;
+  }
 
-
-    @Transactional(isolation = Isolation.READ_UNCOMMITTED)
-    public void loadTest1(Board board){
-
-        board.setTitle("1번");
-        boardRepository.save(board);
-        System.out.println("1번 업데이트 됨");
-
-    }
-
-    @Transactional(isolation = Isolation.READ_UNCOMMITTED)
-    public void loadTest2(Board board){
-
-        board.setTitle("2번");
-        boardRepository.save(board);
-        System.out.println("2번 업데이트 됨");
-
-    }
-
-    @Transactional(isolation = Isolation.READ_COMMITTED)
-    public void loadTest3(Board board){
-
-        board.setTitle("3번");
-        boardRepository.save(board);
-        System.out.println("3번 업데이트 됨");
-
-    }
-
-    @Transactional(isolation = Isolation.READ_COMMITTED)
-    public void loadTest4(Board board){
-
-        board.setTitle("4번");
-        boardRepository.save(board);
-        System.out.println("4번 업데이트 됨");
-
-    }
-
-    @Transactional(isolation = Isolation.REPEATABLE_READ)
-    public void loadTest5(Board board){
-
-        board.setTitle("5번");
-        boardRepository.save(board);
-        System.out.println("5번 업데이트 됨");
-
-    }
-
-    @Transactional(isolation = Isolation.REPEATABLE_READ)
-    public void loadTest6(Board board){
-
-        board.setTitle("6번");
-        boardRepository.save(board);
-        System.out.println("6번 업데이트 됨");
-
-    }
-
-    @Transactional(isolation = Isolation.SERIALIZABLE)
-    public void loadTest7(Board board){
-
-        board.setTitle("7번");
-        boardRepository.save(board);
-        System.out.println("7번 업데이트 됨");
-
-    }
-
-    @Transactional(isolation = Isolation.SERIALIZABLE)
-    public void loadTest8(Board board){
-
-        board.setTitle("8번");
-        boardRepository.save(board);
-        System.out.println("8번 업데이트 됨");
-
-    }
-
-    public void delete(int idx) {
-        boardRepository.deleteById(idx);
-    }
-
+  public void delete(int idx) {
+    boardRepository.deleteById(idx);
+  }
 }
