1dcd0a6c6a9b8436a3ca83ebc7bb434aab8775af
xenex90 <xenexes@naver.com>
2023-11-08 18:24:54 +0900

    Tested runrace test 1,2,3
    동시성 문제인 RunRace 현상 테스트한 코드입니다.
    
    동시성 문제 발생하는 테스트 코드 (주석) - 일반적 코드
    동시성 문제 해결한 테스트 코드1 (Production code) - synchronized
    동시성 문제 해결한 테스트 코드2 (주석) - Sington방식 + synchronized
    
    으로 구성되어 있습니다.

--- everycodeacademy/src/main/java/com/every/everycodeacademy/concurrency/ConcurrencyServiceImpl.java
+++ everycodeacademy/src/main/java/com/every/everycodeacademy/concurrency/ConcurrencyServiceImpl.java
@@ -11,6 +11,8 @@ public class ConcurrencyServiceImpl implements ConcurrencyService {
 
   @Autowired private ConcurrencyRepository concurrencyRepository;
 
+
+  //동시성 문제 해결한 테스트 코드1
   @Override
   public synchronized void incrementCount(Long value) {
 
@@ -29,3 +31,62 @@ public class ConcurrencyServiceImpl implements ConcurrencyService {
     } // 동기화 블록 끝
  // }
 }
+
+/*
+*
+*   @Override    //동시성 문제 발생하는 테스트 코드
+  public void incrementCount(Long value) {
+
+  //  synchronized (lock) { // 동기화 블록 시작
+      Concurrency concurrency = concurrencyRepository.findById(1L).orElseThrow();
+      concurrency.setCount(concurrency.getCount() + 1);
+      concurrencyRepository.save(concurrency);
+ //   } // 동기화 블록 끝
+  }
+
+  @Override
+  public synchronized Long getCount() {
+  //  synchronized (lock) { // 동기화 블록 시작
+      Concurrency concurrency = concurrencyRepository.findById(1L).orElseThrow();
+      return concurrency.getCount();
+    } // 동기화 블록 끝
+ // }
+*
+*
+*
+* //동시성 문제 해결한 테스트 코드2
+* synchronized (lock) { // 동기화 블록 시작
+      Concurrency concurrency = concurrencyRepository.findById(1L).orElseThrow();
+      concurrency.setCount(concurrency.getCount() + 1);
+      concurrencyRepository.save(concurrency);
+    } // 동기화 블록 끝
+  }
+
+  @Override
+  public synchronized Long getCount() {
+    synchronized (lock) { // 동기화 블록 시작
+      Concurrency concurrency = concurrencyRepository.findById(1L).orElseThrow();
+      return concurrency.getCount();
+    } // 동기화 블록 끝
+  }
+*
+*
+*   @Override
+  public synchronized void incrementCount(Long value) {
+
+  //  synchronized (lock) { // 동기화 블록 시작
+      Concurrency concurrency = concurrencyRepository.findById(1L).orElseThrow();
+      concurrency.setCount(concurrency.getCount() + 1);
+      concurrencyRepository.save(concurrency);
+ //   } // 동기화 블록 끝
+  }
+
+  @Override
+  public synchronized Long getCount() {
+  //  synchronized (lock) { // 동기화 블록 시작
+      Concurrency concurrency = concurrencyRepository.findById(1L).orElseThrow();
+      return concurrency.getCount();
+    } // 동기화 블록 끝
+ // }
+*
+* */
