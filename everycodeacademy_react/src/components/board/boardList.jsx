218f1dd663da5750e2b16bb0eb24ec7c5950f3f7
xenex90 <xenexes@naver.com>
2024-04-02 09:32:13 +0900

    초기 게시판 추가
    리스트 및 게시글 추가
    로컬 CORS 허용 추가

---
+++ everycodeacademy_react/src/components/board/boardList.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import boardView from "./boardView";
+
+const BoardList = () => {
+  const [boards, setBoards] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get('http://localhost:8080/board/list');
+        setBoards(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+      <Table striped bordered hover>
+        <thead>
+        <tr>
+          <th>#</th>
+          <th>제목</th>
+          <th>작성자</th>
+          <th>작성일</th>
+        </tr>
+        </thead>
+        <tbody>
+        {boards.map((board, index) => (
+            <tr key={board.idx} ><Link to="/board/view/${board.idx}"> </Link>
+              <td>{index + 1} </td>
+              <td><Link to={`/board/view/${board.idx}`} > {board.title}</Link></td>
+              <td>{board.writername}</td>
+              <td>{board.regDate}</td>
+            </tr>
+        ))}
+        </tbody>
+      </Table>
+  );
+
+};
+
+export default BoardList;
